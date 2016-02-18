8e5781d1af6570be708dbe3d584160956db1154e
Becky Haliscelik <beckym777@gmail.com>
2016-02-17 20:12:01 -0500

    initial files
    Feb17

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+
+/*single page smooth scroll*/
+$('.smooth').on('click', function() {
+    $.smoothScroll({
+        scrollElement: $('body'),
+        scrollTarget: '#' + this.id
+    });
+    
+    return false;
+});
